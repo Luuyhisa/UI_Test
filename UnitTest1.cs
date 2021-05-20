8d7fdb553911f0ebf08c11ca298c21e973930790
Luuyhisa <216060117@mycput.ac.za>
2021-05-20 13:45:02 -0700

    Update

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,12 +1,22 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace QAFrameWork
 {
     public class Tests
     {
-        [SetUp]
+        IWebDriver driver;
+
+
+
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.google.com/";
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
@@ -15,9 +25,24 @@ namespace QAFrameWork
             Assert.Pass();
         }
         [Test]
-        public void Test2()
+        public void GoogleTest()
         {
-            Assert.Fail();
+            //driver.Url = "https://www.google.com/";
+            ////input[@class='gLFyf gsfi']
+            IWebElement inText = driver.FindElement(By.XPath("//input[@class='gLFyf gsfi']"));
+            IWebElement btnEnter = driver.FindElement(By.XPath("//div[@class='FPdoLc lJ9FBc']//input[@class='gNO89b']"));
+            inText.SendKeys("Lungisa MKhasakhasa");
+            inText.SendKeys("\t");
+            btnEnter.Click();
+            //div[@class='lJ9FBc']//input[@class='gNO89b']
+
+            Assert.Pass();
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+           // driver.Close();
         }
     }
 }
