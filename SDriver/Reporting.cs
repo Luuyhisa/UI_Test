f47d340fb5965dc860f3a48b2570c861b47e4035
Luuyhisa <216060117@mycput.ac.za>
2021-05-20 18:17:51 -0700

    Report with screenshots added

---
+++ SDriver/Reporting.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+
+namespace QAFrameWork
+{
+    // public  string passFailScreenshot(string msg)
+    // {
+
+    //     return null;
+    // }
+
+
+
+
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,7 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using AventStack.ExtentReports;
 using AventStack.ExtentReports.Reporter;
-
+using System;
 
 namespace QAFrameWork
 {
@@ -11,60 +11,118 @@ namespace QAFrameWork
     {
         IWebDriver driver;
         ExtentTest test = null;
-
         ExtentReports extent = null;
-
         [OneTimeSetUp]
         public void Setup()
         {
-
-
-
-
             extent = new ExtentReports();
-
             string startupPath = System.IO.Directory.GetDirectoryRoot("Report");
             var htmlReporter = new ExtentHtmlReporter(@"C:\\QAFrameWork\Report\QAReport.html");
-            // var htmlReporter = new ExtentHtmlReporter("C:\\QAFrameWork\\Report\\QAReport.html");
             extent.AttachReporter(htmlReporter);
-
-
-
+        }
+        [SetUp]
+        public void BrowserSetUp()
+        {
             driver = new ChromeDriver();
             driver.Url = "https://www.google.com/";
             driver.Manage().Window.Maximize();
-            
-
-        
         }
-
         [Test]
-        public void Test1()
+        public void EviromentVar()
         {
-            Assert.Pass();
+            extent.AddSystemInfo("Operating System", "Win 10");
+            extent.AddSystemInfo("Host Name", "Computer Name");
+            extent.AddSystemInfo("Browser", "Google Chrome");
         }
+
+
+        public static string ScreenGrab(IWebDriver driver, string ScreenShotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string uptobinPath = path.Substring(0, path.LastIndexOf("bin")) + "Report\\Screenshots\\" + ScreenShotName + ".png";
+            string localPath = new Uri(uptobinPath).LocalPath;
+            screenshot.SaveAsFile(localPath, ScreenshotImageFormat.Png);
+            return localPath;
+        }
+
+
+
+
+
+
         [Test]
         public void GoogleTest()
         {
+            string passFail;
             ExtentTest test = null;
-            test = extent.CreateTest("GoogleTest").Info("Test Started");
-            test.Log(Status.Info, "Browser launched... ");
-            //driver.Url = "https://www.google.com/";
-            ////input[@class='gLFyf gsfi']
-            IWebElement inText = driver.FindElement(By.XPath("//input[@class='gLFyf gsfi']"));
-            IWebElement btnEnter = driver.FindElement(By.XPath("//div[@class='FPdoLc lJ9FBc']//input[@class='gNO89b']"));
-            inText.SendKeys("Lungisa MKhasakhasa");
-            inText.SendKeys("\t");
-            btnEnter.Click();
-            //div[@class='lJ9FBc']//input[@class='gNO89b']
-
-            Assert.Pass();
+            try
+            {
+                test = extent.CreateTest("GoogleTest").Info("Test Started");
+                test.Log(Status.Info, "Browser launched... ");
+                //driver.Url = "https://www.google.com/";
+                ////input[@class='gLFyf gsfi']
+                IWebElement inText = driver.FindElement(By.XPath("//input[@class='gLFyf gsfi']"));
+                IWebElement btnEnter = driver.FindElement(By.XPath("//div[@class='FPdoLc lJ9FBc']//input[@class='gNO89b']"));
+                inText.SendKeys("Lungisa MKhasakhasa");
+                inText.SendKeys("\t");
+                btnEnter.Click();
+                //div[@class='lJ9FBc']//input[@class='gNO89b']
+                test.Log(Status.Pass, "Navigated to the next Page...");
+                //screeenshot
+                string screenshot = ScreenGrab(driver, "Screenshot");
+                test.AddScreenCaptureFromPath(screenshot);
+                passFail = null;
+            }
+            catch (Exception e)
+            {
+                passFail = e.ToString();
+                throw;
+            }
+            finally
+            {
+                if (driver != null) { driver.Quit(); }
+            }
+            Assert.IsNull(passFail, "Failed to complete the Test");
         }
 
-        [OneTimeTearDown]
-        public void Close()
+        [Test]
+        public void GoogleTestFail()
         {
-            // driver.Close();
+            ExtentTest test = null;
+            try
+            {
+                test = extent.CreateTest("GoogleTestFail").Info("Test Started...");
+                test.Log(Status.Info, "Browser launched... ");
+                IWebElement inText = driver.FindElement(By.XPath("//input[@class='111']"));
+                IWebElement btnEnter = driver.FindElement(By.XPath("//div[@class='FPdoLc lJ9FBc']//input[@class='gNO89b']"));
+                inText.SendKeys("Lungisa MKhasakhasa");
+                inText.SendKeys("\t");
+                btnEnter.Click();
+                test.Log(Status.Pass, "Navigated to the next Page...");
+                Assert.Pass();
+            }
+            catch (Exception e)
+            {
+                test.Log(Status.Fail, e.ToString());
+                throw;
+            }
+            finally
+            {
+                if (driver != null) { driver.Quit(); }
+            }
+        }
+
+        [TearDown]
+        public void BrowserTearDown()
+        {
+            //driver.Close();
+        }
+        [OneTimeTearDown]
+        public void Flush()
+        {
+
             extent.Flush();
         }
     }
